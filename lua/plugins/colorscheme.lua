65abb42b9addbd8234d13fcd4beaf210af8fe177
Bartosz Sadowski <bartek_sadowski@o2.pl>
2024-11-21 22:23:46 +0100

    Colorscheme updated

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,18 +1,13 @@
 return {
   {
-    'AlexvZyl/nordic.nvim',
+    'savq/melange-nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      -- vim.cmd.colorscheme 'nordic'
+      vim.opt.termguicolors = true
+      vim.cmd.colorscheme 'melange'
 
       -- Configure highlights
       vim.cmd.hi 'Comment gui=none'
-
-      -- Set colors
-      -- vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#4B6B8F', bold = true })
-      -- vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#4B6B8F', bold = true })
-      vim.api.nvim_set_hl(0, 'Comment', { fg = '#758296' })
-      vim.api.nvim_set_hl(0, '@comment', { link = 'Comment' })
     end,
   },
   -- Highlight todo, notes, etc in comments
