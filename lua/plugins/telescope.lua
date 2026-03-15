64aa037501364d7664b2f50182a91dada8325c5d
Bartosz Sadowski <bartek_sadowski@o2.pl>
2026-03-15 15:58:59 +0100

    fix telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
-    branch = '0.1.x',
+    version = '*',
     dependencies = {
       'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
@@ -15,7 +15,10 @@ return {
 
       { 'nvim-telescope/telescope-ui-select.nvim' },
 
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      {
+        'nvim-tree/nvim-web-devicons',
+        enabled = vim.g.have_nerd_font
+      },
     },
     config = function()
       require('telescope').setup {
